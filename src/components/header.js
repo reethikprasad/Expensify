86ba709c3a0dfefc6f1d8490149161164b8c4664
Reethik Prasad <reethikprasad@gmail.com>
2020-12-09 23:27:07 +0530

    added header

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ const Header = () => (
       Create Expense
     </NavLink>
 
-    <NavLink to="/help" activeClassName="is-active">
+    <NavLink to="/help" activeClassName="is-active">//added
       Help
     </NavLink>
   </header>
