3c106a8a6876f938d954af60b9f6007c40f428d8
Reethik Prasad <reethikprasad@gmail.com>
2021-01-16 12:01:55 +0530

    added edit option

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
-const AddExpensePage = () => (
+import { addExpense } from "../actions/expenses";
+
+const AddExpensePage = (props) => (
   <div>
-    This is from my add expense component
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense));
+        props.history.push("/");
+      }}
+    />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
