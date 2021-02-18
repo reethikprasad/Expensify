1382a9e071d4d22832f6c2474aa6c17b4763298c
Reethik Prasad <reethikprasad@gmail.com>
2021-02-18 19:30:36 +0530

    added remove buttons

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -37,6 +37,17 @@ export const removeExpense = ({ id } = {}) => ({
   id,
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense({ id }));
+      });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: "EDIT_EXPENSE",
@@ -50,7 +61,6 @@ export const setExpenses = (expenses) => ({
   expenses,
 });
 
-// export const startSetExpenses;
 export const startSetExpenses = () => {
   return (dispatch) => {
     return database
