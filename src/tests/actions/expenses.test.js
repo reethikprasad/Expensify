e630531754de9f890487d5c517f908819cd47cda
Reethik Prasad <reethikprasad@gmail.com>
2021-02-24 12:42:39 +0530

    added private routes

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -13,6 +13,8 @@ import {
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
 
+const uid = "thisismytestuid";
+const defaultAuthState = { auth: { uid } };
 const createMockStore = configureMockStore([thunk]);
 
 beforeEach(() => {
@@ -20,7 +22,7 @@ beforeEach(() => {
   expenses.forEach(({ id, description, note, amount, createdAt }) => {
     expensesData[id] = { description, note, amount, createdAt };
   });
-  database.ref("expenses").set(expensesData).then;
+  database.ref(`users/${uid}/expenses`).set(expensesData).then;
 });
 
 test("should setup remove expense action object", () => {
@@ -32,7 +34,7 @@ test("should setup remove expense action object", () => {
 });
 
 test("should remove expense from firebase", () => {
-  const store = createMockStore({});
+  const store = createMockStore(defaultAuthState);
   const id = expenses[2].id;
   store
     .dispatch(startRemoveExpense({ id }))
@@ -42,7 +44,7 @@ test("should remove expense from firebase", () => {
         type: "REMOVE_EXPENSE",
         id,
       });
-      return database.ref(`expenses/${id}`).once("value");
+      return database.ref(`users/${uid}/expenses/${id}`).once("value");
     })
     .then((snapshot) => {
       expect(snapshot.val()).toBeFalsy();
@@ -61,7 +63,7 @@ test("should setup edit expense action object", () => {
 });
 
 test("should edit expense from firebase", () => {
-  const store = createMockStore({});
+  const store = createMockStore(defaultAuthState);
   const id = expenses[0].id;
   const updates = { amount: 21045 };
   store
@@ -73,7 +75,7 @@ test("should edit expense from firebase", () => {
         id,
         updates,
       });
-      return database.ref(`expenses/${id}`).once("value");
+      return database.ref(`users/${uid}/expenses/${id}`).once("value");
     })
     .then((snapshot) => {
       expect(snapshot.val().amount).toBe(updates.amount);
@@ -89,7 +91,7 @@ test("should setup add expense action object with provided values", () => {
 });
 
 test("should add expense to database and store", () => {
-  const store = createMockStore({});
+  const store = createMockStore(defaultAuthState);
   const expenseData = {
     description: "Mouse",
     amount: 3000,
@@ -109,7 +111,9 @@ test("should add expense to database and store", () => {
         },
       });
 
-      return database.ref(`expenses/${actions[0].expense.id}`).once("value");
+      return database
+        .ref(`users/${uid}/expenses/${actions[0].expense.id}`)
+        .once("value");
     })
     .then((snapshot) => {
       expect(snapshot.val()).toEqual(expenseData);
@@ -117,7 +121,7 @@ test("should add expense to database and store", () => {
 });
 
 test("should add expense with defaults to database and store", () => {
-  const store = createMockStore({});
+  const store = createMockStore(defaultAuthState);
   const expenseDefaults = {
     description: "",
     amount: 0,
@@ -137,7 +141,9 @@ test("should add expense with defaults to database and store", () => {
         },
       });
 
-      return database.ref(`expenses/${actions[0].expense.id}`).once("value");
+      return database
+        .ref(`users/${uid}/expenses/${actions[0].expense.id}`)
+        .once("value");
     })
     .then((snapshot) => {
       expect(snapshot.val()).toEqual(expenseDefaults);
@@ -153,7 +159,7 @@ test("should setup set expense action object with data", () => {
 });
 
 test("should fetch the expenses from firebase", (done) => {
-  const store = createMockStore({});
+  const store = createMockStore(defaultAuthState);
   store.dispatch(startSetExpenses()).then(() => {
     const actions = store.getActions();
     expect(actions[0]).toEqual({
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import toJSON from "enzyme-to-json";
-import Header from "../../components/Header";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
+import { Header } from "../../components/Header";
 
 test("should render Header correctly", () => {
   const wrapper = shallow(<Header startLogout={() => {}} />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should call startLogout on button click", () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find("button").simulate("click");
+  expect(startLogout).toHaveBeenCalled();
 });
